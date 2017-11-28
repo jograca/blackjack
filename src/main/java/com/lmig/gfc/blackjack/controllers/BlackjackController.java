125e969cf8cb6e7e086eca7a39b958d342b1bc74
Jon Graca <jon.graca@libertymutual.com>
2017-11-27 21:29:38 -0500

    Checking in Additional functions to make BlackJack Class do the needful

--- src/main/java/com/lmig/gfc/blackjack/controllers/BlackjackController.java
+++ src/main/java/com/lmig/gfc/blackjack/controllers/BlackjackController.java
@@ -11,43 +11,48 @@ import com.lmig.gfc.blackjack.models.BlackJack;
 public class BlackjackController {
 
 	// Private Variables
+	// Setup a Blackjack variable
 	private BlackJack blackjack;
 
 	// Constructor
+	// Setup a Blackjack object
 	public BlackjackController() {
 		this.blackjack = new BlackJack();
 	}
 
 	// Method to return a redirect back to home
 	private ModelAndView redirectToHome() {
+		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("redirect:/");
 		return mv;
 	}
 
 	// GetMapping for the home page (game.html)
+	// addObject for Blackjack
 	@GetMapping("/")
 	public ModelAndView setupGame() {
+		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("game");
 		mv.addObject("blackjack", blackjack);
-		
+
 		return mv;
 	}
 
+	// Deal
+	// Tells bloackjack to setup a Deck, shuffle and deal
 	@PostMapping("/deal")
 	public ModelAndView dealCards() {
-		blackjack.setupDeck();
-		blackjack.shuffle();
-		blackjack.initialDeal();
-		
+		blackjack.startGame();
 		return redirectToHome();
 	}
-	
+
+	// Hit
+	// Tells blackjack it wants a card
 	@PostMapping("/hit")
 	public ModelAndView takeACard() {
 		blackjack.hit();
-		
 		return redirectToHome();
 	}
 }
--- src/main/java/com/lmig/gfc/blackjack/models/Money.java
+++
@@ -1,9 +0,0 @@
-package com.lmig.gfc.blackjack.models;
-
-public class Money {
-
-	public Money() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
